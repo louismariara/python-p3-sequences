0b7f667b930941b02b0cace337312d1a3a5f6762
Louis Githogori Mariara <louismariara@gmail.com>
2024-12-02 12:24:13 +0300

    sequences corrected

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    my_fibonacci=[0,1]
+    if length == 0:
+        print("[]")
+    elif length == 1:
+        print("[0]")
+    else:
+        for i in range(2, length):
+            my_fibonacci.append(my_fibonacci[i-1]+my_fibonacci[i-2])
+        print(my_fibonacci)
+print_fibonacci(10)
